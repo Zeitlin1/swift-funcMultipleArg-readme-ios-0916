e60d093b90423e793603c5a7ea869160eecbbca8
Zeitlin1 <Zeitlin1@gmail.com>
2016-09-20 09:59:46 -0400

    Add files via upload
    Update code to show number of planets as variables

---
+++ Contents.swift
@@ -0,0 +1,56 @@
+func moonsOfJupiter() {
+    print("There are 67 moons orbiting Jupiter.")
+}
+
+moonsOfJupiter()
+
+
+// planetDescription, take 1
+// The bit of code below is commented out, since we iterate on this definition of planetDescription() below.
+
+func planetDescription(planet: String) {
+    print("There are 67 moons orbiting \(planet).")
+}
+
+planetDescription(planet: "Jupiter")
+var planet = "Mars"
+planetDescription(planet: planet)
+
+
+func greet(name: String, greeting: String) {
+    print("\(greeting), \(name)!")
+}
+
+
+func planetDescription(planet: String, numberOfMoons: Int) {
+    print("There are \(numberOfMoons) moons orbiting \(planet)")
+}
+
+// This line of code gives an error.
+// Uncomment it to see what the error is!
+
+planetDescription(planet: "Jupiter", numberOfMoons: 67)
+
+planetDescription(planet: "Jupiter", numberOfMoons: 67)
+
+planet = "Jupiter"
+var moons = 67
+planetDescription(planet: planet, numberOfMoons: moons)
+planet = "Mars"
+moons = 2
+planetDescription(planet: planet, numberOfMoons: moons)
+
+
+planet = "Earth"
+moons = 1
+planetDescription(planet: planet, numberOfMoons: moons)
+
+
+planet = "Venus"
+moons = 3
+planetDescription(planet: planet, numberOfMoons: moons)
+
+
+planet = "Saturn"
+moons = 11
+planetDescription(planet: planet, numberOfMoons: moons)
